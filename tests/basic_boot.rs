56d80dd14ac0ef98d62cd350b087a68eeffbbbb0
Michael Xu <1378606230@qq.com>
2022-09-24 00:57:00 +0800

    Add Integration test in tests/basic_boot.rs and move most of the functionalities into lib.rs

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -1,32 +1,29 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(os_in_rust::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+#![test_runner(os_in_rust::test_runner)]
 
 use core::panic::PanicInfo;
 use os_in_rust::println;
 
-#[no_mangle]
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-/// This function is called on panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
+fn test_runner(tests: &[&dyn Fn()]) {
+    unimplemented!();
 }
 
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     os_in_rust::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output")
 }
