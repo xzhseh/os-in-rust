94f8c5b4578c6b4978dc6b0b2a70e8545bdf55e3
Michael Xu <1378606230@qq.com>
2022-09-23 22:18:13 +0800

    Add some features for test part, now test is available

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
 
@@ -22,6 +25,8 @@ pub extern "C" fn _start() -> ! {
     // write!(vga_buffer::WRITER.lock(), " some numbers: {} {}", 42, 1.337).unwrap();
 
     println!("Hello, World!");
+    #[cfg(test)]
+    test_main();
 
     loop {
 
@@ -30,7 +35,23 @@ pub extern "C" fn _start() -> ! {
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {
 
     }
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
